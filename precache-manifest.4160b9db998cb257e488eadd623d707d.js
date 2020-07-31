7cd7e867098888b3720a01c7a7babbf8ea733969
your name <your email>
2020-07-31 01:13:06 +0100

    Updates

---
+++ precache-manifest.4160b9db998cb257e488eadd623d707d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "141adc56193fa1d191b3d62d1ed8bc34",
+    "url": "/rolodex/index.html"
+  },
+  {
+    "revision": "71ee179dcefa813e62bc",
+    "url": "/rolodex/static/css/main.dfc406e1.chunk.css"
+  },
+  {
+    "revision": "c0eabb28404661e7f77e",
+    "url": "/rolodex/static/js/2.bf376988.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/rolodex/static/js/2.bf376988.chunk.js.LICENSE"
+  },
+  {
+    "revision": "71ee179dcefa813e62bc",
+    "url": "/rolodex/static/js/main.4fe3ea44.chunk.js"
+  },
+  {
+    "revision": "32bb644e3764cfa551e5",
+    "url": "/rolodex/static/js/runtime-main.05ff32b7.js"
+  },
+  {
+    "revision": "86cd5610230b4be8ee498950e9f0f26d",
+    "url": "/rolodex/static/media/oval.86cd5610.svg"
+  }
+]);
